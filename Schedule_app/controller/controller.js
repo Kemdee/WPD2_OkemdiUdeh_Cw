0155b8fdb005c285c167eec4f6ab5583bc60594d
Kemdee <oudeh17@alustudent.com>
2020-11-08 23:58:14 +0100

    Project Functionality version 1

--- Schedule_app/controller/controller.js
+++ Schedule_app/controller/controller.js
@@ -1,6 +1,29 @@
 const guestbookDAO = require('../model/model');
 const db = new guestbookDAO('./Persistence/projects.db');
 
-exports.dashboard = function(req, res) {
-    res.send('Welcome to my application');
+exports.login = function(req, res) {
+    res.sendFile(path.join(public, 'login.html'));
+}
+
+/* exports.post_name = function(req, res) {
+    console.log('processing user');
+
+    db.addEntry(req.body.name);
+    res.redirect('/login/:name');
+}
+*/
+
+exports.show_projects = function(req, res) {
+    res.render('projects');
+}
+
+exports.new_project = function(req, res) {
+    res.render('newProject');
+}
+
+exports.post_new_project = function(req, res) {
+    console.log('processing post-new_project controller');
+
+    db.addEntry(req.body.title, req.body.module1, req.body.due);
+    res.redirect('/');
 }
--- Schedule_app/routes/routes.js
+++ Schedule_app/routes/routes.js
@@ -2,7 +2,17 @@ const express = require('express');
 const controller = require('../controller/controller');
 const router = express.Router();
 
-router.get('/', controller.dashboard);
+router.get('/', (req, res) => {
+    res.redirect('login.html');
+});
+
+//router.post('/', controller.post_name);
+
+router.get('/projects', controller.show_projects);
+
+router.get('/new', controller.new_project);
+
+router.post('/new', controller.post_new_project);
 
 router.use(function(req, res) {
     res.status(404);
@@ -10,10 +20,10 @@ router.use(function(req, res) {
     res.send('404 not found');
 })
 
-router.use(function(err, req, res, next) {
+router.use(function(err, req, res, next){
     res.status(500);
     res.type('text/plain');
-    res,send('Internal server error');
+    res.send('Internal Server Error');
 })
 
 module.exports = router;
