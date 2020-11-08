c76b71163c3a68fc61ddffef2260011eaf5b0e48
Kemdee <oudeh17@alustudent.com>
2020-11-08 21:20:57 +0100

    Basic MVC setup and persistence setup

--- Schedule_app/controller/controller.js
+++ Schedule_app/controller/controller.js
@@ -1,5 +1,5 @@
 const guestbookDAO = require('../model/model');
-const db = new guestbookDAO('../Persistence/projects.db');
+const db = new guestbookDAO('./Persistence/projects.db');
 
 exports.dashboard = function(req, res) {
     res.send('Welcome to my application');
